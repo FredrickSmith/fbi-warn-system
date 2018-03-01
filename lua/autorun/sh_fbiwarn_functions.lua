dc2110cf7adfd489068116e9b518e54fc899a3ba
Algore <actuallyfbi@protonmail.com>
2018-03-01 10:29:44 -0700

    removed ban function
    Replacing it soon

--- lua/autorun/sh_fbiwarn_functions.lua
+++ lua/autorun/sh_fbiwarn_functions.lua
@@ -10,10 +10,6 @@ function ply:AddWarn()
     if (self:GetWarns() == FBIWarn.MaxWarns or self:GetWarns() == FBIWarn.MaxWarns *2) then 
         self:Kick("You've reached the warn limit!")
     end
-    if (self:GetWarns() > 10) then
-        self:ChatPrint("BANNED TO MANY WARNS")
-        self:Ban(30, true)
-    end
     self:SetNWInt("Warnings", self:GetNWInt("Warnings") + 1)
 end
 
