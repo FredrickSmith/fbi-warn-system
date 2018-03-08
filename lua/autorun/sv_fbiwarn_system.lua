f9e388a8356b8347d9d0a9e685a2f3bdcdd25b41
Algore <actuallyfbi@protonmail.com>
2018-03-08 15:45:52 -0700

    Update sv_fbiwarn_system.lua

--- lua/autorun/sv_fbiwarn_system.lua
+++ lua/autorun/sv_fbiwarn_system.lua
@@ -1,19 +1,19 @@
 localfunction InitializePlayerData(ply)
 	
-	// Check if the PData doesn't exist
+	-- Check if the PData doesn't exist
 		
 	if (ply:GetPData("Warnings") == nil) then
 			
-        // Create the PData and NWInt
+        -- Create the PData and NWInt
 			
 		ply:SetPData("Warnings", 0)
 		ply:SetNWInt("Warnings", 0)
 			
-	// Else, if it does exist..
+	-- Else, if it does exist..
 				
 	else
 				
-		// Just create the NWInt
+		-- Just create the NWInt
 				
         ply:SetNWInt("Warnings", ply:GetPData("Warnings"))
 	end
@@ -22,25 +22,25 @@ hook.Add("PlayerInitialSpawn", "InitializePlayerData", InitializePlayerData)
 
 local function SaveDisconnectedPlayerData(ply)
 			
-	// Update the player's warn PData
+	-- Update the player's warn PData
 		
 	if not IsValid(ply) then return end // Something went wrong
 	
     ply:SetPData("Warnings", ply:GetNWInt("Warnings"))
 end
-hook.Add("PlayerDisconnected", "SaveDisconnectedPlayerData", SaveDisconnectedPlayerData)
-hook.Add("PlayerWarnsUpdated", "SaveWarnsOnUpdated", SaveDisconnectedPlayerData)
+hook.Add("PlayerDisconnected", "WARN:SaveDisconnectedPlayerData", SaveDisconnectedPlayerData)
+hook.Add("PlayerWarnsUpdated", "WARN:SaveWarnsOnUpdated", SaveDisconnectedPlayerData)
 	
 local function SaveAllPlayerData(ply)
 			
-	// Loop through every player
+	-- Loop through every player
 			
 	for k, v in pairs(player.GetAll()) do
 				
-		// Update their warnings
+		-- Update their warnings
 				
  		v:SetPData("Warnings", v:GetNWInt("Warnings"))
 	end
 end
 		
-hook.Add("ShutDown", "SaveAllPlayerData", SaveAllPlayerData)
+hook.Add("ShutDown", "WARN:SaveAllPlayerData", SaveAllPlayerData)
